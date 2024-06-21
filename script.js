ac327dba3a41ea7dfb77a4d070c5ef0a1083a66a
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2024-06-21 17:16:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -151,18 +151,15 @@ function displaySkillsInfo(skillsData) {
         skillsTable.appendChild(row);
 
         // 스킬 각인 요소의 텍스트 색상을 검정색으로 변경(해결안됨)
-        // const skillInfoCell = row.querySelector('td:first-child');
-        // skillInfoCell.style.color = 'black';
+        const skillInfoCell = row.querySelector('td:first-child');
+        skillInfoCell.style.color = 'black';
         const skillInfoCell = row.querySelector('td:first-child');
         if (skillInfoCell) {
             skillInfoCell.style.color = 'black';
         } else {
             console.error('Could not find the first <td> element in the row.');
         }
-
-        document.body.style.color = 'black';    // 전체 문서의 텍스트 색상을 검정색으로 변경
-
-        
+        document.body.style.color = 'black';    // 전체 문서의 텍스트 색상을 검정색으로 변경  
     });
 
     characterInfo.appendChild(skillsTable);
@@ -201,6 +198,7 @@ function displayArmoryCardInfo(cardsData) {
 
         const cardGrade = document.createElement('p');
         cardGrade.textContent = `등급: ${card.Grade}`;
+        
 
         cardInfo.appendChild(cardName);
         cardInfo.appendChild(cardGrade);
